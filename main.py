b8f00873a023780a31d74bd67604faf9a5605e3a
Sangam Shikerkar <sangamshikerkar1@gmail.com>
2026-04-14 12:45:02 +0530

    data injestion adeed

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from src.mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
 logger.info('welcome to our custome logging')
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
